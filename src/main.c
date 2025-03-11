5ea5b3b8e077ef9e1e6728eed09fe2618f19d2a1
Vyshnikava Tatsiana <146949961+Moriarte228@users.noreply.github.com>
2025-03-11 07:40:11 +0000

    Prefinished output of result

--- src/main.c
+++ src/main.c
@@ -29,5 +29,9 @@ int main ()
 			class1[i] = 150 + rand() % 51; // Рост от 150 до 200 см
 			class2[i] = 150 + rand() % 51;
 	}
-
+	int third_heights_class1 = find_third_heights(class1);
+	int third_heights_class2 = find_third_heights(class2);
+	printf("Третий по высоте в классе 1: %d см\n", third_heights_class1);
+	printf("Третий по высоте в классе 2: %d см\n", third_heights_class2);
+	
 }
