b212d0cb7a486d24f24695f3043275ca849cc563
Vyshnikava Tatsiana <146949961+Moriarte228@users.noreply.github.com>
2025-03-11 07:32:33 +0000

    Function added

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
-/* hello.c */
+/* main.c */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define CLASS_SIZE 23
+
+int find_third_heights(int height[]) {
+	for(int i = 0; i < 3; i++) {
+		int ind_max = i;
+		for (int j = ind_max + 1; j < CLASS_SIZE; j++) {
+			if (height[j] > height[ind_max])
+				ind_max = j;
+		}
+		int temp = height[i];
+		height[i] = height[ind_max];
+		height[ind_max] = temp;
+	}
+	return height[2];
+}
 
 void main (void)
 {
